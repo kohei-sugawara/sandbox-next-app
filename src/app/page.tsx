b6c949994bcbae3d304ea60a2b76f87da032d6a4
kohei-sugawara <kohei.sugawara@clipline.jp>
2023-12-10 08:00:22 +0900

    Add unused import and update link label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line unused-imports/no-unused-imports, @typescript-eslint/no-unused-vars
+import { unstable_serialize } from 'swr'; // ✅ Available in server components
+import Link from '@/components/ui/Link';
 import Image from 'next/image';
 import React from 'react';
 
@@ -41,6 +44,7 @@ const Home = () => {
       </div>
 
       <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
+        <Link label='すすむ' next='/daisyui' />
         <a
           href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
@@ -48,7 +52,7 @@ const Home = () => {
           rel='noopener noreferrer'
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Document{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
             </span>
